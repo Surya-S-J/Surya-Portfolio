688772d19bd6513953afc237bd1e6e9170fc8d61
Surya <sjsurya2710@gmail.com>
2025-03-31 21:31:26 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,7 +35,7 @@ particlesJS('particles-js',
           "nb_sides": 5
         },
         "image": {
-          "src": "assets/img/github.svg",
+          "src": "assets/github.svg",
           "width": 100,
           "height": 100
         }
