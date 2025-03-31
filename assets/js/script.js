8e2c68aaeb902baac01754f61c1c905509b2b6a3
Surya <sjsurya2710@gmail.com>
2025-03-31 13:36:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | CodeWithTanveer";
+            document.title = "Portfolio | Surya S J";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
         else {
-            document.title = "Portfolio | Tanveer Ahmad";
+            document.title = "Portfolio | Surya S J";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
     });
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development"],
+    strings: ["Electrical and Electronics Engineer", "Graphic design", "Quality Assurance"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
