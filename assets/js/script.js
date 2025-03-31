80e9da658ba9bf7e6b291c56d895d9f169441495
Surya <sjsurya2710@gmail.com>
2025-03-31 19:36:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development"],
+    strings: ["Electrical and Electronics Engineer", "Graphic design", "Quality Assurance"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
